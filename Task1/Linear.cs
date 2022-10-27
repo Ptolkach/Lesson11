d9434d640e518d9a19261eb6c2b50cfa483deee3
Pavel Tolkach <pb.tolkach@outlook.com>
2022-10-27 13:04:48 +0300

    Добавьте файлы проекта.

---
+++ Task1/Linear.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Linear
+    {
+        double k;
+        double b;
+
+        public Linear(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0 && b == 0)
+            {
+                return "любое число (бесконечное число решений)";
+            }
+            else if (b == 0)
+            {
+                return "0";
+            }
+            else 
+            {
+                return $"{-b / k}";
+            }
+
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Для уравнения 0 = k*x -b, задайте вещественные коэффициенты k и b");
+            double k = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Linear linear = new Linear(k, b);
+
+            Console.WriteLine("Решение: {0}", linear.Root());
+            Console.ReadKey();
+        }
+    }
+}
